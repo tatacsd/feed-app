646eada3a8af4c67259a8c4e547ffb50ff1e255b
thayscasado <thayscasado@hotmail.com>
2022-11-14 13:59:25 -0800

    adds header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,10 @@
+import logo from "../../assets/logo.svg";
 import css from "./Header.module.css";
 
 export function Header() {
   return (
     <header className={css.header}>
+      <img src={logo} alt="logo" />
       <strong >Ignite Feed</strong>
     </header>
   );
